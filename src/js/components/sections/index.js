e2be004ce715b370e8df7215ebb5bf9116dc1789
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-04-27 02:14:27 +0200

    Refactoring

--- src/js/components/sections/index.js
+++ src/js/components/sections/index.js
@@ -2,21 +2,17 @@ import { Section } from 'components/ui';
 import {
   GameEnrollment,
   GameEnrollmentForm,
-  GameLineup,
   NewGame,
   NewPitch,
   NewUser,
   PitchesList,
-  PitchInfo,
   UserProfile
 } from 'components/codeball';
 
 export const GameEnrollmentSection = Section(GameEnrollment);
 export const GameEnrollmentFormSection = Section(GameEnrollmentForm);
-export const GameLineupSection = Section(GameLineup);
 export const NewGameSection = Section(NewGame);
 export const NewPitchSection = Section(NewPitch);
 export const NewUserSection = Section(NewUser);
 export const PitchesListSection = Section(PitchesList);
-export const PitchInfoSection = Section(PitchInfo);
 export const UserProfileSection = Section(UserProfile);
