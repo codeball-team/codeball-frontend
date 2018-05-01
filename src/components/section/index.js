d7332cce1677122c705e1c2d2fd1799a66be7009
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2018-05-01 04:54:48 +0200

    Refactoring

--- src/components/section/index.js
+++ src/components/section/index.js
@@ -1,53 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'underscore';
 import { noop } from 'utils';
 import { CancelButton, EditButton, SaveButton, ButtonsPanel, Render } from 'components';
 import styles from './styles.scss';
 
 const CreateSection = (ChildComponent) => {
-  const Section = (props) => {
-    const {
-      buttons,
-      canEdit,
-      canSubmit = true,
-      isEditable,
-      isEditing,
-      title,
-      onCancel = noop,
-      onEdit = noop,
-      onSave = noop
-    } = props;
-    const childProps = {
-      ..._(props).omit(_(Section.propTypes).keys()),
-      isEditing
-    };
-
-    return (
-      <div className={styles.section}>
-        <div className={styles.sectionBar}>
-          <div className={styles.sectionTitle}>
-            {title}
-          </div>
-
-          <ButtonsPanel className={styles.buttonsPanel}>
-            <Render when={canEdit && isEditable && !isEditing}>
-              <EditButton onClick={onEdit} />
-            </Render>
-
-            <Render when={canEdit && isEditable && isEditing}>
-              <CancelButton onClick={onCancel} />
-              <SaveButton isDisabled={!canSubmit} onClick={onSave} />
-            </Render>
-
-            {buttons}
-          </ButtonsPanel>
+  const Section = ({
+    buttons,
+    canEdit,
+    canSubmit = true,
+    isEditable,
+    isEditing,
+    title,
+    onCancel = noop,
+    onEdit = noop,
+    onSave = noop,
+    ...childProps
+  }) => (
+    <div className={styles.section}>
+      <div className={styles.sectionBar}>
+        <div className={styles.sectionTitle}>
+          {title}
         </div>
 
-        <ChildComponent {...childProps} />
+        <ButtonsPanel className={styles.buttonsPanel}>
+          <Render when={canEdit && isEditable && !isEditing}>
+            <EditButton onClick={onEdit} />
+          </Render>
+
+          <Render when={canEdit && isEditable && isEditing}>
+            <CancelButton onClick={onCancel} />
+            <SaveButton isDisabled={!canSubmit} onClick={onSave} />
+          </Render>
+
+          {buttons}
+        </ButtonsPanel>
       </div>
-    );
-  };
+
+      <ChildComponent isEditing={isEditing} {...childProps} />
+    </div>
+  );
 
   Section.propTypes = {
     buttons: PropTypes.node,
